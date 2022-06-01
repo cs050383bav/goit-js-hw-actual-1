37855191dbf1eac7be456a9d9565db3bd25a66e0
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-06-01 22:12:34 +0300

    4

---
+++ js/script-2.js
@@ -0,0 +1,14 @@
+const totalPrice = 562.50;
+console.log('Цена', totalPrice);
+const isOpen = true;
+
+
+const tipe = typeof "fgfhfh 5";
+const tipe2 = typeof 5;
+const tipe3 = typeof isOpen;
+
+console.log(tipe,tipe2,tipe3);
+
+console.log('До');
+alert('stop');
+console.log(5);
