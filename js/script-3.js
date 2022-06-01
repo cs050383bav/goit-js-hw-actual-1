c2e92ca7b9287dd836804666a9482f78ac395737
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-06-01 22:37:00 +0300

    33

---
+++ js/script-3.js
@@ -0,0 +1,9 @@
+
+// const message = 'Подовжити підписку?';
+// const shouldRenew = confirm(message);
+// console.log(shouldRenew)
+
+let quantaty = prompt("Введіть кількість товару");
+// quantaty = Number(quantaty); -- для того щоб зробити строку числом;
+console.log(quantaty);
+console.log(typeof quantaty);
